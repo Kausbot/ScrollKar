04efa340abdcd85072b4787dc30049397cb124e6
Kausbot <f2014420@goa.bits-pilani.ac.in>
2016-07-01 13:49:10 +0530

    One scroll adder

--- app/src/main/java/ps1/bits_piliani/scrollkar/MainActivity.java
+++ app/src/main/java/ps1/bits_piliani/scrollkar/MainActivity.java
@@ -2,12 +2,20 @@ package ps1.bits_piliani.scrollkar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.text.method.ScrollingMovementMethod;
+import android.widget.TextView;
+import android.widget.RelativeLayout;
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView mytext = (TextView)findViewById(R.id.myText);
+        mytext.setMovementMethod(new ScrollingMovementMethod());
+
+
+
     }
 }
